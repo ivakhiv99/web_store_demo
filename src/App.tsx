6bd0abaf1132f1575ce11d12ae22caaec65a3ef4
Vitaliy <ivahiv99@gmail.com>
2023-07-07 11:55:07 +0300

    initial commit - project setup

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    hello world
+    </>
+  )
+}
+
+export default App
+
+
+// TODO:
+
+// Pages:  
+// - product list
+// - product details
+// - shoping cart
+// - checkout 
+
+// Functionality: 
+// - fetch list of products
+// * add a filter for displaying products?
+// - fetch info about product
+// - add product to cart
+// - change quantity of products in cart
+// - delete products from cart
+// - calculate price
+// - collect and display checkout info
+
+// Other:
+// - find product api
+
+//TIME LOG:
+
+// 07.07.2023
+// start 11:45 -   
